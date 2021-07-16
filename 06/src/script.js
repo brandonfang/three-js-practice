d82c28fa09a39afb885d3aeb1755b07bdea22505
brandonfang <bdmfang@gmail.com>
2021-07-16 14:00:00 -0700

    Use Clock and add GSAP

--- 06/src/script.js
+++ 06/src/script.js
@@ -1,5 +1,6 @@
 import "./style.css";
 import * as THREE from "three";
+import gsap from "gsap";
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
@@ -32,17 +33,21 @@ renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
 
 // Animate
-let time = Date.now();
+// const clock = new THREE.Clock();
+gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 });
 
 const tick = () => {
-  // Calculate delta time
-  const currentTime = Date.now();
-  const deltaTime = currentTime - time;
-  time = currentTime;
+  // const elapsedTime = clock.getElapsedTime();
 
   // Update objects
-  mesh.rotation.y += 0.01 * deltaTime;
-  
+  // mesh.rotation.y = elapsedTime;
+  // mesh.position.x = Math.cos(elapsedTime);
+  // mesh.position.y = Math.sin(elapsedTime);
+
+  // camera.position.x = Math.cos(elapsedTime);
+  // camera.position.y = Math.sin(elapsedTime);
+  // camera.lookAt(mesh.position);
+
 
   // Render
   renderer.render(scene, camera);

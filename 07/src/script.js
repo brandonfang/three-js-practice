ccd28f78fd2cafc2874fd261cd4a82214b90e206
brandonfang <bdmfang@gmail.com>
2021-07-17 13:00:00 -0700

    Add basic camera controls

--- 07/src/script.js
+++ 07/src/script.js
@@ -24,12 +24,27 @@ const mesh = new THREE.Mesh(
 scene.add(mesh);
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.x = 2;
-camera.position.y = 2;
-camera.position.z = 2;
+const aspectRatio = sizes.width / sizes.height;
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 1, 100);
+// const camera = new THREE.OrthographicCamera(-1 * aspectRatio, 1 * aspectRatio, 1, -1, 0.1, 100);
+// camera.position.x = 2;
+// camera.position.y = 2;
+camera.position.z = 3;
 camera.lookAt(mesh.position);
 scene.add(camera);
+
+// Cursor
+const cursor = {
+  x: 0,
+  y: 0
+};
+
+window.addEventListener('mousemove', (event) => {
+  cursor.x = event.clientX / sizes.width - 0.5;
+  cursor.y = -(event.clientY / sizes.height - 0.5);
+
+  console.log(cursor.x, cursor.y);
+});
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -45,6 +60,15 @@ const tick = () => {
 
   // Update objects
   mesh.rotation.y = elapsedTime;
+
+  // Update camera
+  // camera.position.x = cursor.x;
+  // camera.position.y = cursor.y;
+
+  camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 2;
+  camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 2;
+  camera.position.y = cursor.y * 3;
+  camera.lookAt(mesh.position);
 
   // Render
   renderer.render(scene, camera);

2d34d4e3f9c23a8bbaff8e448343e5934d7d393f
brandonfang <bdmfang@gmail.com>
2021-07-21 12:00:00 -0700

    Create buffer geometry

--- 09/src/script.js
+++ 09/src/script.js
@@ -12,8 +12,30 @@ const canvas = document.querySelector('canvas.webgl');
 const scene = new THREE.Scene();
 
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+// Create an empty BufferGeometry
+const geometry = new THREE.BufferGeometry();
+
+// Create a Float32Array containing the vertices position (3 by 3)
+// const positionsArray = new Float32Array([
+//     0, 0, 0, // First vertex
+//     0, 1, 0, // Second vertex
+//     1, 0, 0  // Third vertex
+// ])
+// Create 50 triangles (450 values)
+const count = 50;
+const positionsArray = new Float32Array(count * 3 * 3);
+for (let i = 0; i < count * 3 * 3; i++) {
+  positionsArray[i] = (Math.random() - 0.5) * 4;
+}
+
+// Create the attribute and name it 'position'
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3);
+geometry.setAttribute('position', positionsAttribute);
+
+const material = new THREE.MeshBasicMaterial({
+  color: 0xff0000,
+  wireframe: true,
+});
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 

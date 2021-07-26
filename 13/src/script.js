85f56003679cefc9f9505a5b12852208f8351919
brandonfang <bdmfang@gmail.com>
2021-07-26 13:00:00 -0700

    Randomize position, rotation, and scale of donuts

--- 13/src/script.js
+++ 13/src/script.js
@@ -46,9 +46,18 @@ fontLoader.load('/fonts/helvetiker_regular.typeface.json', (font) => {
   const text = new THREE.Mesh(textGeometry, material);
   scene.add(text);
 
+  const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45);
+  
   for (let i = 0; i < 100; i++) {
-    const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45);
     const donut = new THREE.Mesh(donutGeometry, material);
+    donut.position.x = (Math.random() - 0.5) * 10;
+    donut.position.y = (Math.random() - 0.5) * 10;
+    donut.position.z = (Math.random() - 0.5) * 10;
+    donut.rotation.x = Math.random() * Math.PI;
+    donut.rotation.y = Math.random() * Math.PI;
+    const scale = Math.random();
+    donut.scale.set(scale, scale, scale);
+    
     scene.add(donut);
   }
 });
